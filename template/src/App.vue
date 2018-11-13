5fbabaa3c3110606ea431638740ea1212e2ea36b
ln-bates <lnbates@sina.com>
2018-11-13 18:56:51 +0800

    change indent

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,22 +1,22 @@
 <template>
-<div id="app">
-    {{#router}}
-    <router-view></router-view>
-    {{else}}
-    <Home></Home>
-    {{/router}}
-</div>
+    <div id="app">
+        {{#router}}
+        <router-view></router-view>
+        {{else}}
+        <Home></Home>
+        {{/router}}
+    </div>
 </template>
 
 <script>
-{{#unless router}}
-import Home from './pages/Home'
+    {{#unless router}}
+    import Home from './pages/Home'
 
-{{/unless}}
-export default {
-    name: 'App'{{#router}}{{else}},
-    components: {
-        Home
-    }{{/router}}
-}
+    {{/unless}}
+    export default {
+        name: 'App'{{#router}}{{else}},
+        components: {
+            Home
+        }{{/router}}
+    }
 </script>
